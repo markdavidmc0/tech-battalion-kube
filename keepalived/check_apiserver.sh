55aacb538c6c8c3e5f15f35ab818b52e368864c6
Mark Mc Naught <markdavidmc0@gmail.com>
2022-06-19 23:27:31 +0000

    Update 443 -> 6443 for check_apiserver.sh.

--- keepalived/check_apiserver.sh
+++ keepalived/check_apiserver.sh
@@ -5,7 +5,7 @@ errorExit() {
     exit 1
 }
 
-curl --silent --max-time 2 --insecure https://localhost:443/ -o /dev/null || errorExit "Error GET https://localhost:443/"
+curl --silent --max-time 2 --insecure https://localhost:6443/ -o /dev/null || errorExit "Error GET https://localhost:6443/"
 if ip addr | grep -q $10.240.10.10; then
-    curl --silent --max-time 2 --insecure https://10.240.10.10:443/ -o /dev/null || errorExit "Error GET https://10.240.10.10:443/"
+    curl --silent --max-time 2 --insecure https://10.240.10.10:6443/ -o /dev/null || errorExit "Error GET https://10.240.10.10:6443/"
 fi
